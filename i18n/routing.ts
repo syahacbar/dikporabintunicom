b07c162887bef39e2e924d0029ec71a096ea9ab3
Papa Ara <syahacbar@gmail.com>
2026-06-29 01:25:34 +0900

    default locale

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -5,6 +5,7 @@ export const routing = defineRouting({
   locales: ["id", "en"],
   defaultLocale: "id",
   localePrefix: "always",
+  localeDetection: false,
 });
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
 import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${routing.defaultLocale}`;
+    return NextResponse.redirect(url);
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
   matcher: ["/", "/(id|en)/:path*"],
